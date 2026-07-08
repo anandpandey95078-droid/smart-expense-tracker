a3411bac5f0fef574f7a45b5c81d38fdc8cc1e36
Anand Pandey <anandpandey95078@gmail.com>
2026-07-08 23:19:09 +0530

    Initial commit

---
+++ src/main/java/com/anand/expansetracker/SmartExpanseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.anand.expansetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpanseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpanseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anand/expansetracker/SmartExpanseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.expansetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpanseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
